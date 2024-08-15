e96eac19bd49e59432f4b8036a18a19fcf3081ef
SayuriMalaviarachchi <164402934+SayuriMalaviarachchi@users.noreply.github.com>
2024-08-16 00:27:29 +0530

    Add css to Add user page

--- frontend/src/Components/AddUser/AddUser.js
+++ frontend/src/Components/AddUser/AddUser.js
@@ -1,86 +1,60 @@
-import React, { useState } from 'react'
-import Nav from '../Nav/Nav'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState } from 'react';
+import Nav from '../Nav/Nav';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styles from './AddUser.module.css'; // Import the CSS module
 
 export default function SignUp() {
+  const history = useNavigate();
 
-const history = useNavigate()
+  const [inputs, setUser] = useState({
+    name: "",
+    password: "",
+    gmail: "",
+    phone: "",
+    address: "",
+  });
 
-  const [inputs,setUser] = useState({//calling data set
-    name:"",
-    password:"",
-    gmail:"",
-    phone:"",
-    address:"",
-  })
-
-  const handleChange =(e) =>{
+  const handleChange = (e) => {
     setUser((prevState) => ({
-    ...prevState,
-    [ e.target.name]:  e.target.value
-    }))
-  }
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
 
-  const handleSubmit =(e) =>{//what will happen when submit button is pressed.
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
-    sendRequest().then(()=>history('/mainhome'))//redirecting page
-   // 2 different ways
-  }
+    sendRequest().then(() => history('/mainhome'));
+  };
 
-  const sendRequest = async() =>{
-    await axios.post("http://localhost:5000/users",{
-      name:String(inputs.name),
-      password:String(inputs.password),
-      gmail:String(inputs.gmail),
-      phone:Number(inputs.phone),
-      address:String(inputs.address),
-    }).then(res => res.data)
-  } 
+  const sendRequest = async () => {
+    await axios.post("http://localhost:5000/users", {
+      name: String(inputs.name),
+      password: String(inputs.password),
+      gmail: String(inputs.gmail),
+      phone: Number(inputs.phone),
+      address: String(inputs.address),
+    }).then(res => res.data);
+  };
+
   return (
-
-    <div>
-
-      <Nav/>
-
+    <div className={styles.container}> {/* Apply container class */}
+      <Nav />
       <h1>Add user</h1>
-
       <form onSubmit={handleSubmit}>
-
         <label>Name</label>
-        <br></br>
-        <input type = "text" name="name" onChange={handleChange} value={inputs.name} required></input>
-        <br></br>
-        <br></br>
-
+        <input type="text" name="name" onChange={handleChange} value={inputs.name} required />
         <label>Password</label>
-        <br></br>
-        <input type = "Password" name="password" onChange={handleChange}  value={inputs.password}  required></input>
-        <br></br>
-        <br></br>
-
+        <input type="password" name="password" onChange={handleChange} value={inputs.password} required />
         <label>Gmail</label>
-        <br></br>
-        <input type = "gmail" name="gmail" onChange={handleChange}  value={inputs.gmail}  required></input>
-        <br></br>
-        <br></br>
-
+        <input type="email" name="gmail" onChange={handleChange} value={inputs.gmail} required />
         <label>Phone</label>
-        <br></br>
-        <input type = "tel" name="phone" onChange={handleChange}  value={inputs.phone}  required></input>
-        <br></br>
-        <br></br>
-
+        <input type="tel" name="phone" onChange={handleChange} value={inputs.phone} required />
         <label>Address</label>
-        <br></br>
-        <input type = "text" name="address" onChange={handleChange}  value={inputs.address}  required></input>
-        <br></br>
-        <br></br>
-
-        <button>Add User</button>
+        <input type="text" name="address" onChange={handleChange} value={inputs.address} required />
+        <button type="submit">Add User</button>
       </form>
-
     </div>
-  )
-} 
+  );
+}
